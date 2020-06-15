2d3e03ef047fb7c99faa1615309c022e88c5ceb3
generocha <gene@generocha.com>
2020-06-15 14:44:07 -0700

    add sass and style folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
+import "./styles/style.scss";
 
 const App = () => {
   return <h1>Hello!!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   devtool: "cheap-module-eval-source-map",
