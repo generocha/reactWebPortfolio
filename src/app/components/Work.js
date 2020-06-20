2480f609bebd0fe0e04c6b7d05f7a980c44c9f0f
generocha <gene@generocha.com>
2020-06-19 18:55:24 -0700

    add portfolio items

--- src/app/components/Work.js
+++ src/app/components/Work.js
@@ -9,13 +9,61 @@ export class Work extends React.Component {
         <p class="section__subtitle section__subtitle--work">
           A selection of my range of work
         </p>
-
         <div class="portfolio">
-          {/* Portfolio item 01 */}
-          <a href="portfolio-item.html" class="portfolio__item">
-            <p>test1</p>
-            <img src="" alt="" class="portfolio__img" />
-          </a>
+          <div class="portoflio__item">
+            <img
+              class="portfolio__img"
+              src="images/2020_garden.png"
+              alt="Card image cap/"
+            />
+            <div class="card-body">
+              <h3 class="card-title">2020 Garden</h3>
+              <p class="card-text">2020 Garden bar chat built with D3.</p>
+              <a
+                href="https://github.com/generocha/2020Garden"
+                class="btn btn-primary"
+              >
+                github
+              </a>
+              <a
+                href="http://www.generocha.com/2020Garden/"
+                class="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+          <div class="portoflio__item">
+            <img
+              class="portfolio__img"
+              src="images/google_maps_breweries.png"
+              alt="Card image cap/"
+            />
+            <div class="card-body">
+              <h5 class="card-title">
+                Custom Google Map of my favorite Breweries
+              </h5>
+              <p class="card-text">
+                This is a custom Google Map built with the Google Maps API and
+                Google Sheets API.
+              </p>
+              <a
+                href="https://github.com/generocha/favoriteBreweriesMap"
+                class="btn btn-primary"
+                target="_blank"
+              >
+                github
+              </a>
+              <a
+                href="http://www.generocha.com/favoriteBreweriesMap/"
+                class="btn btn-primary"
+                target="_blank"
+              >
+                live demo
+              </a>
+            </div>
+          </div>
         </div>
       </section>
     );
